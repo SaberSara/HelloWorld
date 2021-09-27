326ca36daf202afb0c126ee925d3bcedb76e55e2
Sara SABER <sarasaberdev@gmail.com>
2021-09-27 13:03:55 +0300

    Add .gitIgnore created from toptal website and ignored the bin and obj files

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+
+//In an other story:
+//Chapter 18:
+//"Actually…
+//Since.NET 5, we don‘t even need to specify a class with a Main Method anymore
+//For simple programs, we can just write the code we want to execute
+//Outside of a class:
+//System.Console.WriteLine("Hello World!");"
+
+//Chapter 19:
+//"19.Now, this is what I call a proper "Hello World!"
+//System.Console.WriteLine("Hello World!");"
